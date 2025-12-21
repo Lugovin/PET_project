4771abbb562a83312185488f183c09717c19787b
Nikolay <lugovin@gmail.com>
2025-12-21 12:38:47 +0300

    рефактор

--- src/main/java/org/example/pet_project/PetProjectApplication.java
+++ src/main/java/org/example/pet_project/PetProjectApplication.java
@@ -2,12 +2,11 @@ package org.example.pet_project;
 
 
 //import org.example.pet_project.arduino.EthernetControlPanel;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import javax.swing.*;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PetProjectApplication {
@@ -20,12 +19,5 @@ public class PetProjectApplication {
         ConfigurableApplicationContext context =
                 SpringApplication.run(PetProjectApplication.class, args);
 
-//        // Получаем GUI bean из контекста
-//        EthernetControlPanel gui = context.getBean(EthernetControlPanel.class);
-//
-//        // Показываем GUI
-//        SwingUtilities.invokeLater(() -> {
-//            gui.setVisible(true);
-//        });
     }
 }
